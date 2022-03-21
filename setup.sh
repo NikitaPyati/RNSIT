c55124c3443f7021bede3491eb6bd3cf039af948
NikitaPyati <91679577+NikitaPyati@users.noreply.github.com>
2022-03-21 12:21:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
-headless=true\n\
+headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-" > ~/.streamlit/congif.toml
+" > ~/.streamlit/config.toml
